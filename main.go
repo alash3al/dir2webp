710f5e6b30aa481004d7d765a3ed6bb3019e5ba5
Mohammed Al Ashaal <m7medalash3al@gmail.com>
2017-11-15 09:54:08 +0200

    Update main.go

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func main() {
 				wg.Add(1)
 				go func() {
 					defer wg.Done()
-					convert(path, path + ".webp", *CLEAN)
+					convert(path, re.ReplaceAllString(path, ".webp"), *CLEAN)
 				}()
 				return nil
 			})
